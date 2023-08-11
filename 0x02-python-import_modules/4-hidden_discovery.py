b8d6f94f4cc3d4e3777f0fa63817f31c65d2d05b
Sark-o07 <kosinick01.gmail.com>
2023-08-11 09:00:11 -0700

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     name_arr = dir(hidden_4)
-    arr_len = len(name_arr)
-
     for name in name_arr:
         if (name[:2] != "__"):
             print(name)
