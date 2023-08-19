b4365f2290eb9c32e5190ee441a8e552e340bf5f
Sark-o07 <kosinick01.gmail.com>
2023-08-18 17:22:05 -0700

    15. Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary.items():
-        if v == value:
-            del k
+    for k in  a_dictionary:
+        if a_dictionary[k] == value:
+            del a_dictionary[k]
     return a_dictionary
