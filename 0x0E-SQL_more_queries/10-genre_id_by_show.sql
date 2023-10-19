bccae0b32191381769bb5b29fe73ebe61e7d27b2
Sark <kosinick01@gmail.com>
2023-10-19 16:42:23 +0100

    Mandatory Task Done

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
-i-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
 -- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
