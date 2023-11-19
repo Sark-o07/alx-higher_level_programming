1ac2d68fc91137de8a878913f08952c54c8cd18d
Sark <kosinick01@gmail.com>
2023-11-19 20:17:21 +0100

    ORM Scripts!

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that lists all cities from the database given"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        INNER JOIN states ON states.id=cities.state_id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (match,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
